7eaf16bd08c3e337710b70b943555b8e7db5c559
crazyjaco <crazyjac0@gmail.com>
2017-01-29 20:30:43 -0500

    Removing javascript relying on jquery. Reworking the menus this depends on anyway.

--- src/templates/partials/page-head.php
+++ src/templates/partials/page-head.php
@@ -12,52 +12,6 @@
 	wp_head();
 ?>
 
-
-
-  <script language="javascript" type="text/javascript">
-    var intTimeout    = 500;
-    var hdlCloseTimer = null;
-    var objSubMenu    = null;
-
-    function PowetMenu_OpenMenu(){
-      PowetMenu_CancelTimer();
-      PowetMenu_CloseMenu();
-      
-      objSubMenu = $(this).find('ul').css('visibility', 'visible');
-    }
-    function PowetMenu_CloseMenu(){
-      if(objSubMenu){
-        objSubMenu.css('visibility', 'hidden');
-      }
-    }
-
-    function PowetMenu_SetTimer(){
-      hdlCloseTimer = window.setTimeout(PowetMenu_CloseMenu, intTimeout);
-    }
-    function PowetMenu_CancelTimer(){
-      if(hdlCloseTimer){
-        window.clearTimeout(hdlCloseTimer);
-        hdlCloseTimer = null;
-      }
-    }
-
-    $(document).ready(
-      function(){
-        // Open menu from link
-        $('#PowetMenu > li').bind('mouseover', PowetMenu_OpenMenu)
-        // Turn on timer when leaving link
-        $('#PowetMenu > li').bind('mouseout',  PowetMenu_SetTimer)
-        
-        // Open menu from link
-        $('#NewsMenu > li').bind('mouseover', PowetMenu_OpenMenu)
-        // Turn on timer when leaving link
-        $('#NewsMenu > li').bind('mouseout',  PowetMenu_SetTimer)
-      }
-    );
-
-    document.onclick = PowetMenu_CloseMenu;
-  </script>
-
   <script type="text/javascript">
   function recordOutboundLink(link, category, action) {
     try {
