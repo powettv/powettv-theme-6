12b0462933cd08e5f14cb462ec9c7276a2a506af
crazyjaco <crazyjac0@gmail.com>
2017-01-29 20:32:02 -0500

    Removed old footer.

--- src/templates/partials/page-footer.php
+++ src/templates/partials/page-footer.php
@@ -5,14 +5,3 @@
 		</div>
 	</div>
 </footer>
-
-<table cellpadding="0" cellspacing="0" width="1000px">
-  <!-- Footer -->
-  <tr>
-    <td style="vertical-align: bottom; height: 50px;" background="http://powet.tv/powetblog/wp-content/themes/Powet35/images/312/menu_main_bg.jpg">
-      <div style="color: #FFFFFF; font-weight: bold; margin: 5px;">
-        &copy; 2017 Powet.TV
-      </div>
-    </td>
-  </tr>
-</table>
